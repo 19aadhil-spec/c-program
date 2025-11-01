37956e40a60198ec9191c36a6170cfd1ff2cc0e3
19aadhil-spec <19aadhil@gmail.com>
2025-11-01 10:38:03 +0530

    Add files via upload

---
+++ day2 q1.cpp
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(){
+    char ch='A';
+    printf("ASCII value of %c=%d",ch,ch);
+    return 0;
+}
---
+++ day2 q2.cpp
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main (){
+     int ascii =66;
+     printf("Character for ASCII %d=%c",ascii,ascii);
+     return 0;
+ }
